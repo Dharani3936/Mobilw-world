ca35dd5c444f621959ee335698a78fad1bab2664
Dharani3936 <113873410+Dharani3936@users.noreply.github.com>
2022-09-19 11:19:23 +0530

    Add files via upload

---
+++ mobile world/src/auto/login.java
@@ -0,0 +1,28 @@
+package auto;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class login {
+
+	
+	@Test
+	public void setup() throws InterruptedException{
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\bhavadharani.p\\OneDrive - Qualitest Group\\driver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		pom login=new pom(driver);
+		login.goTo();
+		login.loginpage("bhavadharani", "q1w2e3");
+
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\bhavadharani.p\\OneDrive - Qualitest Group\\driver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver1=new ChromeDriver();
+		driver1.manage().window().maximize();
+		pom login1=new pom(driver1);
+		login1.goTo();
+		login1.loginpage("", "");
+	}
+
+}
---
+++ mobile world/src/auto/mobiles.java
@@ -0,0 +1,28 @@
+package auto;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class mobiles {
+
+	
+	@Test
+	public void setup() throws InterruptedException{
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\bhavadharani.p\\OneDrive - Qualitest Group\\driver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		pom all=new pom(driver);
+		all.goTo();
+		all.mobiles("samsung");
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\bhavadharani.p\\OneDrive - Qualitest Group\\driver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver1=new ChromeDriver();
+		driver1.manage().window().maximize();
+		pom all1=new pom(driver1);
+		all1.goTo();
+		all1.mobiles("samsung 20 pixel with dual camera ");
+	}
+
+}
